c11a8e393744b8283e4ddd352d2c5d4f2e2075f8
Harrison Cramer <harrisoncramer@gmail.com>
2020-10-08 19:40:09 -0400

    added pageview logging for google analytics

--- packages/frontend/src/history/index.ts
+++ packages/frontend/src/history/index.ts
@@ -1,4 +1,11 @@
 import { createBrowserHistory } from "history";
+import ReactGA from "react-ga";
 
 const history = createBrowserHistory();
+
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname }); // Update the user's page
+  ReactGA.pageview(location.pathname); // Record a pageview for page
+});
+
 export default history;
