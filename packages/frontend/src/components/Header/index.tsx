fe05ea44c05fbd266838b55a1ffda074cfeb1d2a
Harrison Cramer <harrisoncramer@gmail.com>
2020-10-18 21:50:00 -0400

    moved header meta tags to static html

--- packages/frontend/src/components/Header/index.tsx
+++ packages/frontend/src/components/Header/index.tsx
@@ -25,44 +25,7 @@ const Header = (): React.ReactElement => {
           content="The webscraper for busy people on Capitol Hill."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* Open-graph Tags */}
         <meta name="title" property="og:title" content="Cloture" />
-        <meta
-          name="image"
-          property="og:image"
-          content="https://storage.googleapis.com/cloture/Cloture_Social.png"
-        />
-        <meta
-          name="description"
-          property="og:description"
-          content="The webscraper for busy people on Capitol Hill."
-        />
-        <meta name="url" property="og:url" content="https://www.cloture.app" />
-        {/* Social Tags for Twitter */}
-        <meta
-          name="twitter:card"
-          content="The webscraper for busy people on Capitol Hill."
-        />
-        <meta name="twitter:site" content="@ClotureApp" />
-        <meta
-          name="twitter:title"
-          content="The webscraper for busy people on Capitol Hill."
-        />
-        <meta
-          name="twitter:description"
-          content="Cloture lets you stay up-to-date on Capitol Hill."
-        />
-        <meta
-          name="twitter:image"
-          content="https://storage.googleapis.com/cloture/Cloture_Social.png"
-        />
-        <meta name="twitter:site" content="@ClotureApp" />
-        <meta name="twitter:creator" content="@harrisoncramer" />
-        {/* Other tags (location) */}
-        <meta name="geo.region" content="US" />
-        <meta name="geo.placename" content="Washington D.C." />
-        <meta name="geo.position" content="38.889803;-77.009418" />
-        <meta name="ICBM" content="38.889803, -77.009418" />
       </Helmet>
       <nav>
         <IconButton
